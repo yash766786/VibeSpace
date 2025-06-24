c1160c1a0859850a44f582a459a82dfb886eb70a
yash766786 <yashchoudhary766786@gmail.com>
2025-06-24 20:11:59 +0530

    updating cookie option --testing

--- Server1/src/constant/constant.js
+++ Server1/src/constant/constant.js
@@ -2,21 +2,24 @@
 const cookieOptions = {
     httpOnly: true,
     secure: true,
-    sameSite: "None", // For cross-site cookies
+    sameSite: "None", // 🔥 Required for cross-origin cookies
+    domain: ".onrender.com", // 🔥 Enables access across Server1 & Server2
     maxAge: 24 * 60 * 60 * 1000, // 1 day
 };
 
 const cookieOptionsForResetPassword = {
     httpOnly: true,
     secure: true,
-    sameSite: "None", // For cross-site cookies
+    sameSite: "None", // 🔥 Required for cross-origin cookies
+    domain: ".onrender.com", // 🔥 Enables access across Server1 & Server2
     maxAge: 10 * 60 * 60, // 10 min
 };
 
 const deleteCookieOptions = {
     httpOnly: true,
     secure: true,
-    sameSite: "None", // For cross-site cookies
+    sameSite: "None", // 🔥 Required for cross-origin cookies
+    domain: ".onrender.com", // 🔥 Enables access across Server1 & Server2
     expires: 0,
 };
 
