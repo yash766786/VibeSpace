84f41f4af4e6202540c4febffc0e2d6fa3d227b6
anshulvyasa <anshulvtech121@gmail.com>
2025-09-07 05:45:10 +0000

    web socket url updated

--- Client/src/conf/conf.js
+++ Client/src/conf/conf.js
@@ -1,7 +1,7 @@
 const conf = {
   server1Url: 'https://vibespacehttp.techynimbus.com/api/v3',
   server2Url: 'https://vibespaceio.techynimbus.com/api/v3',
-  server2Socket: 'https://vibespaceio.techynimbus.com/',
+  server2Socket: 'wss://vibespaceio.techynimbus.com/', 
 }
 
 const configWithHeaders = {
